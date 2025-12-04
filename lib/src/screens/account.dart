9be5f55fbeba616f8231a6d624a0e97e1db02486
sackflorencia <florenciasack123@gmail.com>
2025-12-04 17:19:55 -0300

    Agregué fluttertoast al pubspec.yaml

--- lib/src/screens/account.dart
+++ lib/src/screens/account.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Account extends StatelessWidget {
   const Account({super.key});
@@ -7,11 +8,34 @@ class Account extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Bienvenido a Nava!",
-        style: TextStyle(
-          fontSize: 32,
-          fontWeight: FontWeight.w700
-        ),),
+        child: Column(children: [
+          Text("¡Bienvenido a Nava!",
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.w700,
+              color: Color.fromRGBO(44, 44, 44, 1)
+            ),
+          ),
+          Text("Navegá tus objetivos",
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.w500,
+              color: Color.fromRGBO(107, 107, 107, 1)
+            )
+          ),
+          Text("Sign in"),
+          ElevatedButton(
+            onPressed: () {
+                Fluttertoast.showToast(
+                  msg: "Esta función todavía no está disponible",
+                );
+              },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Color.fromRGBO(255, 216, 94, 1),
+            ),
+            child: Text('Sign up'))
+        ],
+        ),
       ),
     );
   }
